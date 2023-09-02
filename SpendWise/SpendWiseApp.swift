62a11a647cfb88fbaf40a84e2332c808d69290f2
Sachini Perera <sachiper200@gmail.com>
2023-09-02 15:55:20 +0530

    initial commit

---
+++ SpendWise/SpendWiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpendWiseApp.swift
+//  SpendWise
+//
+//  Created by Sachini Perera on 2023-09-01.
+//
+
+import SwiftUI
+
+@main
+struct SpendWiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
