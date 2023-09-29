3baf10fad7ecc31c6ef7e352a4642b35b7e3b309
Sachini Perera <sachiper200@gmail.com>
2023-09-29 11:06:52 +0530

    email colour bug fix

--- SpendWise/View/TabSettingsView.swift
+++ SpendWise/View/TabSettingsView.swift
@@ -24,6 +24,7 @@ struct TabSettingsView: View {
                         .font(.title3)
                         .frame(maxWidth: .infinity)
                         .tint(.primary)
+                        .foregroundColor(.primary)
                         .bold()
                     ,
                     content: {}
